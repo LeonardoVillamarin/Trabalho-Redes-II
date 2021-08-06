be948c7f1b25679fd4013afeb491502381b275bc
Leonardo Villamarin <62810234+LeonardoVillamarin@users.noreply.github.com>
2021-08-05 21:16:41 -0300

    Inclusão Tkinter

--- connect.py
+++ connect.py
@@ -1,29 +1,65 @@
 import socket
+from tkinter import *
 
-HOST = input("IP do Servidor: ")   # Endereco IP do Servidor
-PORT = 5000            # Porta que o Servidor esta
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-dest = (HOST, PORT)
-tcp.connect(dest)
 
-tcp.sendall(('{"Registro":"'+socket.gethostname()+'"}').encode())
-
-resp = tcp.recv(1024)
-print(resp.decode())
-
-opcao = int(input("1. Buscar usuário\n2. Desconectar\n"))
-if opcao == 1:
-        nomeUsuario = input("Digite o nome do usuário: ")
-        msg = '{"Consulta":"'+nomeUsuario+'"}'
-        tcp.sendall(msg.encode())
+def conn():
+        HOST = str(ip.get()).split(":")[1].strip()
+        print(HOST)
+        PORT = 5000            # Porta que o Servidor esta
         
-while opcao != 2:
-    resp = tcp.recv(1024)
-    print(resp.decode())
-    opcao = int(input("1. Buscar usuário\n2. Desconectar\n"))
-    if opcao == 1:
-        nomeUsuario = input("Digite o nome do usuário: ")
-        msg = '{"Consulta":"'+nomeUsuario+'"}'
+        button.destroy()
+        ip.destroy()
+        
+        dest = (HOST, PORT)
+        tcp.connect(dest)
+
+        tcp.sendall(('{"Registro":"'+socket.gethostname()+'"}').encode())
+
+        resp = tcp.recv(1024)
+        text["text"] = resp.decode()
+        text.after(2000, text.destroy)
+
+        global usuario
+        usuario = Entry(window, width=50, bg="white", fg="black")
+        usuario.pack(padx=100, pady=20)
+        usuario.insert(0, "Digite o nome do usuário: ")
+
+        button2 = Button(window, text="Enviar", command=sendMsg)
+        button2.pack(padx=10, pady=10, side=LEFT)
+
+        button3 = Button(window, text="Desconectar", command=closeConn)
+        button3.pack(padx=10, pady=10,side=RIGHT)
+
+def sendMsg():
+        msg = '{"Consulta":"'+usuario.get().split(":")[1].strip()+'"}'
         tcp.sendall(msg.encode())
-tcp.sendall(('{"Encerrar":"'+socket.gethostname()+'"}').encode())
-tcp.close()
+
+        text = Label(window, text="")
+        text.pack()
+
+        resp = tcp.recv(1024)
+        text["text"] = resp.decode()
+
+        text.after(5000, text.destroy)
+
+def closeConn():
+        tcp.sendall(('{"Encerrar":"'+socket.gethostname()+'"}').encode())
+        tcp.close()
+        window.destroy()
+
+window = Tk()
+window.geometry("500x500")
+window.title("Trabalho Part I")
+
+ip = Entry(window, width=50, bg="white", fg="black")
+ip.pack(padx= 100, pady= 30)
+ip.insert(0, "Digite o IP: ")
+
+text = Label(window, text="")
+text.pack(padx= 100, pady= 30)
+button = Button(window, text="Confirmar", command=conn)
+button.pack(padx= 100, pady= 30)
+
+window.mainloop()
+
